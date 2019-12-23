c44149c0481f77fcd87b547266759804afc3262a
willredmo <wil.redmond47@gmail.com>
2019-12-22 20:17:22 -0500

    Basic design done. Next step is smooth scrolling.

---
+++ script.js
@@ -0,0 +1,43 @@
+// Global variables
+var navHidden = true;
+
+$(window).on("load", function() {
+    initViewportHeight();
+    handleMobileNav();
+    handleSmoothScroll();
+});
+
+
+function initViewportHeight() {
+    // Credit: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+}
+
+function handleMobileNav() {
+    var hiddenHeight = 56;
+    var shownHeight = 300;
+
+    $("#navButton").click(function() {
+        if (navHidden) {
+            $(".nav").css("height", shownHeight+"px");
+        } else {
+            $(".nav").css("height", hiddenHeight+"px");
+        }
+        navHidden = !navHidden;
+    });
+}
+
+function handleSmoothScroll() {
+    // TODO
+}   
