db58e7dc897937a52ed98d969c791eb5f94f4887
Will <wil.redmond47@gmail.com>
2020-04-23 18:22:32 -0400

    Cleaned up info and changed up skills section

--- script.js
+++ script.js
@@ -2,27 +2,80 @@
 var navHidden = true;
 var sections = [];
 var experience = [
+    {  
+        heading: "Back-end Contractor",
+        subheading: "LynksIn",
+        skills: [
+            "NodeJS",
+            "TypeScript",
+            "AWS",
+            "Serverless",
+            "MongoDB",
+            "Mongoose",
+            "Git"
+        ],
+        info: [
+            "Info about Lyskin"
+        ],
+        date: "January - April 2020",
+        location: "Remote",
+        image: "images/lynksIn.png"
+    },
     {
         heading: "IT Intern",
         subheading: "Johnson & Johnson",
-        info: "Intern within IT R&D and worked on two Chef cookbooks. The first cookbook which contained Git submodules, automated the authorization of users on a cluster’s execute nodes (AWS EC2 instances) and deployed this cookbook as a Microsoft CycleCloud project. The second cookbook generated a Docker image with Chef Client installed on it within a VPC and utilized a base image retrieved from a private Docker registry.",
-        date: "May 2018 - August 2018",
+        skills: [
+            "AWS",
+            "EC2 Cluster",
+            "Chef",
+            "Docker",
+            "Bash Shell",
+            "Shell Scripting",
+            "Git"
+        ],
+        info: [
+            "Worked in IT R&D to create two Chef cookbooks that maintained a cluster of AWS EC2 instances",
+            "The first cookbook contained Git submodules, automated the authorization of users on a cluster’s execute nodes (AWS EC2 instances), and deployed this cookbook as a Microsoft CycleCloud project",
+            "Wrote another cookbook that generated a Docker image with a Chef Client installed on it within a VPC and utilized a base image retrieved from a private Docker registry"
+        ],
+        date: "May - August 2018",
         location: "Raritan, New Jersey",
         image: "images/j&j.png"
     },
     {
         heading: "Web/UI Video Software Development Intern",
         subheading: "Lenel Systems International",
-        info: "Worked in a team on web video tile for security cameras. Application is built in Typescript and consumes a private video streaming API to display live and recorded video in forward and reverse.",
-        date: "September 2017 - December 2017",
+        skills: [
+            "TypeScript",
+            "Agile Development",
+            "Git"
+        ],
+        info: [
+            "Worked in a team that was tasked with creating web video tile for security cameras that would be part of a larger application",
+            "Coded in TypeScript and received video data from a streaming API developed by the back-end team",
+            "Implemented playing the video in reverse and a buffer that helped with loading video data"
+        ],
+        date: "September - December 2017",
         location: "Pittsford, New York",
         image: "images/lenel.png"
     },
     {
         heading: "Development Intern",
         subheading: "MapAnything",
-        info: "Worked on indoor mapping application for Dreamforce (Salesforce expo). Application was created using HTML, CSS, JavaScript. Retrieved data from Salesforce using REST API.",
-        date: "June 2017 - August 2017",
+        skills: [
+            "JavaScript",
+            "jQuery",
+            "HTML",
+            "CSS",
+            "Salesforce (Apex, Lightning,Visualforce, SOQL, SOSL)",
+            "Git"
+        ],
+        info: [
+            "Worked on an indoor mapping application for Dreamforce (Salesforce expo)",
+            "The application was created using HTML, CSS, JavaScript",
+            "Data was stored and retrieved from Salesforce using a REST API"
+        ],
+        date: "June - August 2017",
         location: "Atlanta, Georgia",
         image: "images/mapanything.png"
     }
@@ -32,19 +85,23 @@ var education = [
     {
         heading: "Rochester Institute of Technology",
         subheading: "Bachelor of Science in Web and Mobile Computing",
-        info: "GPA: 3.93<a target=\"_blank\" href=\"https://meritpages.com/William-Redmond/3115530\">Dean's List</a><a target=\"_blank\" href=\"https://www.rit.edu/academicaffairs/outstanding-undergraduate-scholars/2019-2020-outstanding-undergraduate-scholars\">Outstanding Scholar Award</a>",
+        info: [
+            "GPA: 3.93",
+            "<a target=\"_blank\" href=\"https://meritpages.com/William-Redmond/3115530\">Dean's List</a>",
+            "<a target=\"_blank\" href=\"https://www.rit.edu/academicaffairs/outstanding-undergraduate-scholars/2019-2020-outstanding-undergraduate-scholars\">Outstanding Scholar Award</a>"
+        ],
         date: "September 2015 - December 2019",
         location: "Rochester, New York",
         image: "images/rit.png"
     },
-    {
-        heading: "Cardinal Spellman High School",
-        subheading: "High School Diploma",
-        info: "",
-        date: "September 2011 - May 2015",
-        location: "Brockton, Massachusetts",
-        image: "images/spellman.jpg"
-    }
+    // {
+    //     heading: "Cardinal Spellman High School",
+    //     subheading: "High School Diploma",
+    //     info: "",
+    //     date: "September 2011 - May 2015",
+    //     location: "Brockton, Massachusetts",
+    //     image: "images/spellman.jpg"
+    // }
 ];
 var skills = [
     {
@@ -141,10 +198,24 @@ var projects = [
     {
         heading: "BattleFlood",
         subheading: "Turn Based 2 Player Game",
-        info: `
-        I built this application as project for one of my upper level courses. The front-end is built in JavaScript/HTML/Less and the back-end is built in PHP. The game is one on one where each player takes turns selecting a color and takes control of the adjacent blocks same colors. All players start in the main lobby and move to a separate lobby when they start a match. I have also built from scratch both an account and chat system. I have also made this application mobile accessible.
-        <a target='_blank' href='http://ec2-54-85-190-233.compute-1.amazonaws.com/battleflood/client/login.php'>Link to application</a><a target='_blank' href='https://github.com/willredmo/battleflood'>View Code</a>",
-        `,
+        skills: [
+            "JavaScript",
+            "jQuery",
+            "HTML",
+            "LESS",
+            "PHP",
+            "MySQL",
+            "Mobile Responsive"
+        ],
+        info: [
+            "Built this application as a project for one of my upper level courses",
+            "The front-end is built in JavaScript/HTML/Less and the back-end is built in PHP/MySQL",
+            "Created game logic in the backend that was able to identify adjacent blocks of the same color",
+            "Implemented account, lobby, and chat systems into the game",
+            "Playable with both large and small screens",
+            "<a target='_blank' href='http://ec2-54-85-190-233.compute-1.amazonaws.com/battleflood/client/login.php'>Use application</a>",
+            "<a target='_blank' href='https://github.com/willredmo/battleflood'>View Code</a>"
+        ],
         date: "",
         location: "",
         image: "images/battleflood.png"
@@ -152,9 +223,21 @@ var projects = [
     {
         heading: "Cocktail Project",
         subheading: "Application For New Finding Cocktails",
-        info: `This application is another project for one of my elective courses: Beverage Fermentations and Distillations. I built the application solely in JavaScript/HTML/Less and retrieved the data from a free API called "CocktailDB". The user can search for random cocktails to make or use the multiple types of filters to find the cocktail they desire. This application is also mobile accessible.
-        <a target='_blank' href='http://ec2-54-85-190-233.compute-1.amazonaws.com/battleflood/client/login.php'>Link to application</a><a target='_blank' href='https://github.com/willredmo/cocktailProject'>View Code</a>",
-        `,
+        skills: [
+            "JavaScript",
+            "jQuery",
+            "HTML",
+            "LESS",
+            "Mobile Responsive"
+        ],
+        info: [
+            "Wrote mobile friendly application for one of my elective courses, Beverage Fermentations and Distillations",
+            "Application is composed solely of JavaScript/HTML/Less and retrieved the data from a free API called \"CocktailDB\"",
+            "Designed filters to allow users to find cocktails that match their criteria",
+            "Adjusted for both large and small screens",
+            "<a target='_blank' href='http://ec2-54-85-190-233.compute-1.amazonaws.com/battleflood/client/login.php'>Use application</a>",
+            "<a target='_blank' href='https://github.com/willredmo/cocktailProject'>View Code</a>"
+        ],
         date: "",
         location: "",
         image: "images/cocktail.png"
@@ -168,7 +251,7 @@ $(window).on("load", function() {
     handleSmoothScroll();
     handleActiveLink();
     generateResumeItems();
-    generateSkillItems();
+    // generateSkillItems();
     handleValidateForm();
     generatePersonalCarousel();
 });
@@ -308,10 +391,6 @@ function resizeSkillsHeight() {
             if (contentHeight != currentHeight) {
                 $(this).height(contentHeight+"px");
             }
-
-            // console.log("___________________________________________");
-            // console.log("Initial: "+currentHeight);
-            // console.log("Content: "+contentHeight);
         }
     });
 }
@@ -320,7 +399,29 @@ function generateItem(data) {
     var html = $("#templates .item").clone();
     $(html).find(".content h3").html(data.heading);
     $(html).find(".content h4").html(data.subheading);
-    $(html).find(".content p").html(data.info);
+
+    if (data.hasOwnProperty("skills")) {
+        var skillsHtml = "";
+        for (var i = 0; i < data.skills.length; i++) {
+            skillsHtml += "<span class=\"badge badge-pill badge-primary\">"+data.skills[i]+"</span>";
+        }
+        $(html).find(".content .skills").html(skillsHtml);
+    }
+    
+    var infoHtml = "";
+    if (Array.isArray(data.info)) {
+        infoHtml = "<ul>";
+        var customBullet = "<span class='material-icons'>arrow_right</span>";
+        // Generate Education
+        for (var i = 0; i < data.info.length; i++) {
+            infoHtml += "<li>"+customBullet+data.info[i]+"</li>";
+        }
+        infoHtml += "</ul>";
+    } else {
+        infoHtml = data.info
+    }
+    $(html).find(".content .info").html(infoHtml);
+
     $(html).find(".date").html(data.date);
     $(html).find(".location").html(data.location);
     $(html).find(".img").css("background-image", "url("+data.image+")");
